64cea8482aa2a118bcb595c2d832465c7a989cb4
KyeonghoShin <2158653@slu.edu.ph>
2018-09-24 15:40:39 +0800

    making a extender

--- Convoy.java
+++ Convoy.java
@@ -40,7 +40,7 @@ class State{
         this.cars = cs;
         this.maxWeight = mw;
         this.length = lth;
-        this.time = t;
+        this.time = t;// this is hour, will be converted by adding 60 when it is printed out
         this.Description = d;
     }
 
@@ -51,24 +51,39 @@ class State{
     public ArrayList<State> extend(){
         ArrayList<Car> group = new ArrayList<>();
         ArrayList<State> successors= new ArrayList<>();
-        int currentWeight = 0; Car currentCar; int slowest;
+        int currentWeight = 0; Car currentCar;
 
 
-        for(int i=0 ;i<cars.size() && this.maxWeight < currentWeight ;i++){
+        for(;&& this.maxWeight < currentWeight ;){
 
+            currentCar = cars.remove();//a car is out from queue
 
-            currentCar = cars.remove();
-            group.add(currentCar);
-            currentWeight += currentCar.getWeight();
-            for(i: group){
-
+            //if car is heavier than the bridge can hold , exit and give error message
+            if(currentCar.getWeight()>maxWeight){
+                System.out.println("there is a Car heavier than maximum, No solution is available");
+                System.exit(0);
             }
 
-            successors.add(new State(this.cars,this.maxWeight,this.length,/*time*/,/*desc*/));
+            group.add(currentCar);//car joined on the group
+            String des = "Following cars are joined group : "+currentCar.toString()+ " ";
+            currentWeight += currentCar.getWeight();//weight of group is measured
+
+            //finder of slower
+            int slow = 0;
+            for(int i=0;i<group.size();i++){
+                if (group.get(i).getSpeed()<slow) slow = group.get(i).getspeed;
+            }
+
+            //preprocess
+            double time += this.length/slow;// length divided by speed is time in h this will be
+
+            successors.add(new State(this.cars,this.maxWeight,this.length,time,des));
+            }
+
+            return successors;
         }
 
-        return successors;
+
     }
 
-}
 
